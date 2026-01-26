334c27d1b77f032cb488df85d312dc6e9b550403
Tyler Madonna <tjm159@pitt.edu>
2026-01-26 11:15:31 -0500

    Added support for restoring from manifest in s3

--- cmd/restore.go
+++ cmd/restore.go
@@ -1,13 +1,21 @@
 package cmd
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"runtime"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
-	"github.com/x-atlas-consortia/filebackup/internal/aws"
+	internalaws "github.com/x-atlas-consortia/filebackup/internal/aws"
 	"github.com/x-atlas-consortia/filebackup/internal/core"
 	"github.com/x-atlas-consortia/filebackup/internal/list"
 	"github.com/x-atlas-consortia/filebackup/internal/restore"
@@ -37,12 +45,29 @@ var restoreStartCmd = &cobra.Command{
 			panic("profile not found in context")
 		}
 
+		tempDir, err := cmd.Flags().GetString("temp-dir")
+		if err != nil {
+			return err
+		}
+		if stat, err := os.Stat(tempDir); err != nil || !stat.IsDir() {
+			return errors.New("temp-dir must be a valid directory")
+		}
+
 		// Validate manifest file path
 		manifestPath, err := cmd.Flags().GetString("manifest")
 		if err != nil {
 			return err
 		}
-		manifest, err := aws.ParseManifestFile(manifestPath)
+		if internalaws.IsS3Path(manifestPath) {
+			// Download manifest from S3 to temp location
+			manifestPath, err = downloadManifestFromS3(cmd.Context(), config, manifestPath, tempDir)
+			if err != nil {
+				return err
+			}
+			defer os.Remove(manifestPath)
+		}
+
+		manifest, err := internalaws.ParseManifestFile(manifestPath)
 		if err != nil {
 			return err
 		}
@@ -59,14 +84,6 @@ var restoreStartCmd = &cobra.Command{
 		details, err := cmd.Flags().GetString("details")
 		if err != nil {
 			return err
-		}
-
-		tempDir, err := cmd.Flags().GetString("temp-dir")
-		if err != nil {
-			return err
-		}
-		if stat, err := os.Stat(tempDir); err != nil || !stat.IsDir() {
-			return errors.New("temp-dir must be a valid directory")
 		}
 
 		maxWorkers, err := cmd.Flags().GetInt("max-workers")
@@ -103,8 +120,8 @@ var restoreListCmd = &cobra.Command{
 			return err
 		}
 
-		if aws.IsS3Path(outPath) {
-			bucket, _, err := aws.ParseS3Path(outPath)
+		if internalaws.IsS3Path(outPath) {
+			bucket, _, err := internalaws.ParseS3Path(outPath)
 			if err != nil {
 				return err
 			}
@@ -146,7 +163,7 @@ var restoreStatusCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		manifest, err := aws.ParseManifestFile(manifestPath)
+		manifest, err := internalaws.ParseManifestFile(manifestPath)
 		if err != nil {
 			return err
 		}
@@ -156,8 +173,8 @@ var restoreStatusCmd = &cobra.Command{
 			return err
 		}
 
-		if aws.IsS3Path(outPath) {
-			bucket, _, err := aws.ParseS3Path(outPath)
+		if internalaws.IsS3Path(outPath) {
+			bucket, _, err := internalaws.ParseS3Path(outPath)
 			if err != nil {
 				return err
 			}
@@ -212,4 +229,54 @@ func init() {
 	// out flag
 	restoreStatusCmd.Flags().StringP("out", "o", "", "Path to the output file (default: stdout)")
 	restoreStatusCmd.MarkFlagFilename("out")
+}
+
+// downloadManifestFromS3 downloads the manifest file from S3 to a temporary location.
+func downloadManifestFromS3(ctx context.Context, config core.Config, manifestPath, tempDir string) (string, error) {
+	if !internalaws.IsS3Path(manifestPath) {
+		return "", errors.New("manifest path is not a valid S3 path")
+	}
+
+	bucket, key, err := internalaws.ParseS3Path(manifestPath)
+	if err != nil {
+		return "", err
+	}
+	if bucket != config.AWSS3Bucket {
+		return "", errors.New("manifest S3 bucket does not match configured backup bucket")
+	}
+
+	// Download manifest file to a temp location. Add timestamp to avoid collisions.
+	tempManifestName := fmt.Sprintf("filebackup-backup-%s.tmp", time.Now().UTC().Format("2006-01-02-15-04-05"))
+	tempManifestPath := filepath.Join(tempDir, tempManifestName)
+
+	// Create S3 client
+	s3Client := s3.New(s3.Options{
+		Credentials: credentials.NewStaticCredentialsProvider(config.AWSAccessKeyID, config.AWSSecretAccessKey, ""),
+		Region:      config.AWSRegion,
+	})
+
+	downloader := manager.NewDownloader(s3Client)
+
+	// Create the output file
+	outFile, err := os.Create(tempManifestPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to create output file %s: %w", tempManifestPath, err)
+	}
+	defer outFile.Close()
+
+	// Download the file
+	dlInput := &s3.GetObjectInput{
+		Bucket:       aws.String(config.AWSS3Bucket),
+		ChecksumMode: "ENABLED",
+		Key:          aws.String(key),
+	}
+	_, err = downloader.Download(ctx, outFile, dlInput)
+	if err != nil {
+		// Clean up the partially downloaded file
+		outFile.Close()
+		os.Remove(tempManifestPath)
+		return "", fmt.Errorf("failed to download file %s/%s: %w", config.AWSS3Bucket, key, err)
+	}
+
+	return tempManifestPath, nil
 }
